aa0e153f36d7917edc4480453740cceafe64a1c1
KyoheiYamamoto <arienkyohei@gmail.com>
2020-03-19 20:44:09 +0900

    firebase設定

--- pages/index.vue
+++ pages/index.vue
@@ -13,17 +13,18 @@
 import Messages from '~/components/Messages.vue'
 import ChatForm from '~/components/ChatForm.vue'
 import { db } from '~/plugins/firebase'
+
 export default {
  components: {
    Messages,
    ChatForm
  },
  mounted(){
-  //  channel id を指定して、doc  id を指定します。
-db.collection('channels').doc('O7DJtaAU6nmCxmPIyXdC').get()
-    .then((doc) =>{
-     console.log('doc.id: ', doc.id)
-     console.log('doc.data(): ', doc.data())
- })
-}
+  db.collection('channels').get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach(function(doc) {
+      console.log(doc.id, " => ", doc.data())
+      })
+})
+
 </script>
